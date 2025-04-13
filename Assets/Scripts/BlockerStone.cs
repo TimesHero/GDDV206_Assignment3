d0ea3c7b017f46ed58661fe6839540d32740e35e
Drift King48 <jtlbrion@gmail.com>
2025-04-13 13:12:42 -0400

    force push

--- Assets/Scripts/BlockerStone.cs
+++ Assets/Scripts/BlockerStone.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class BlockerCurlingStone : CurlingStone
 {
-    // Override to disable burning behavior completely
     public override void TryBurn()
     {
-        // No burning effect for the blocker stone.
         if (fireParticles.activeInHierarchy) 
         {
-            fireParticles.SetActive(false);  // Ensure fire particles are never activated.
+            fireParticles.SetActive(false);
         }
 
         burning = false;  
